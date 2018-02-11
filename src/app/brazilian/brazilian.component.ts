da37bedc3bf67f6889f45ee8db17bfb1fd0df75b
Vinicius <vsampaio@ice.ufjf.br>
2018-02-11 15:41:49 -0200

    set table of fees

--- src/app/brazilian/brazilian.component.ts
+++ src/app/brazilian/brazilian.component.ts
@@ -9,11 +9,12 @@ import { Http } from '@angular/http/src/http';
 export class BrazilianComponent implements OnInit {
 
   BrazilData:any = [];
+  BrazilFee:any = [];
 
   constructor(private http: Http) {
     this.http.get('https://cors-anywhere.herokuapp.com/https://api.bitvalor.com/v1/ticker.json').subscribe(data => {
       let dataGeral = data.json();
-      console.log(dataGeral.ticker_24h.exchanges)
+      // console.log(dataGeral)
       this.BrazilData.push(dataGeral.ticker_24h.exchanges.B2U);
       this.BrazilData.push(dataGeral.ticker_24h.exchanges.BTD);
       this.BrazilData.push(dataGeral.ticker_24h.exchanges.BZX);
@@ -22,18 +23,35 @@ export class BrazilianComponent implements OnInit {
       this.BrazilData.push(dataGeral.ticker_24h.exchanges.LOC);
       this.BrazilData.push(dataGeral.ticker_24h.exchanges.MBT);
       this.BrazilData.push(dataGeral.ticker_24h.exchanges.NEG);
-      Object.assign(this.BrazilData[0],{name:'B2U'});
-      Object.assign(this.BrazilData[1],{name:'BTD'});
-      Object.assign(this.BrazilData[2],{name:'BZX'});
-      Object.assign(this.BrazilData[3],{name:'FLW'});
-      Object.assign(this.BrazilData[4],{name:'FOX'});
-      Object.assign(this.BrazilData[5],{name:'LOC'});
-      Object.assign(this.BrazilData[6],{name:'MBT'});
-      Object.assign(this.BrazilData[7],{name:'NEG'});
+      Object.assign(this.BrazilData[0],{name:'BitcoinToYou'});
+      Object.assign(this.BrazilData[1],{name:'BitcoinTrade'});
+      Object.assign(this.BrazilData[2],{name:'Braziliex'});
+      Object.assign(this.BrazilData[3],{name:'flowBTC'});
+      Object.assign(this.BrazilData[4],{name:'Foxbit'});
+      Object.assign(this.BrazilData[5],{name:'LocalBitcoins'});
+      Object.assign(this.BrazilData[6],{name:'Mercado Bitcoin'});
+      Object.assign(this.BrazilData[7],{name:'Negocie Coins'});
+    });
+
+    this.http.get('https://cors-anywhere.herokuapp.com/https://api.bitvalor.com/v1/exchanges.json').subscribe(data => {
+      let dataGeral = data.json();
+      console.log(dataGeral)
+      this.BrazilFee.push(dataGeral.ARN);
+      this.BrazilFee.push(dataGeral.B2U);
+      this.BrazilFee.push(dataGeral.BAS);
+      this.BrazilFee.push(dataGeral.BIV);
+      this.BrazilFee.push(dataGeral.BSQ);
+      this.BrazilFee.push(dataGeral.BTD);
+      this.BrazilFee.push(dataGeral.BZX);
+      this.BrazilFee.push(dataGeral.FLW);
+      this.BrazilFee.push(dataGeral.FOX);
+      this.BrazilFee.push(dataGeral.LOC);
+      this.BrazilFee.push(dataGeral.MBT);
+      this.BrazilFee.push(dataGeral.NEG);
+      this.BrazilFee.push(dataGeral.PAX);
     });
 
 
-    console.log('BrazilData',this.BrazilData);
   }
 
   ngOnInit() {
