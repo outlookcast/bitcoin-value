97759919a501afc181b6d57d19635a5b67246c57
Vinicius <vsampaio@ice.ufjf.br>
2018-02-11 16:38:59 -0200

    hotfix

--- src/app/brazilian/brazilian.component.ts
+++ src/app/brazilian/brazilian.component.ts
@@ -9,48 +9,34 @@ import { Http } from '@angular/http/src/http';
 export class BrazilianComponent implements OnInit {
 
   BrazilData:any = [];
-  BrazilFee:any = [];
 
   constructor(private http: Http) {
-    this.http.get('https://cors-anywhere.herokuapp.com/https://api.bitvalor.com/v1/ticker.json').subscribe(data => {
-      let dataGeral = data.json();
-      // console.log(dataGeral)
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.B2U);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.BTD);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.BZX);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.FLW);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.FOX);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.LOC);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.MBT);
-      this.BrazilData.push(dataGeral.ticker_24h.exchanges.NEG);
-      Object.assign(this.BrazilData[0],{name:'BitcoinToYou'});
-      Object.assign(this.BrazilData[1],{name:'BitcoinTrade'});
-      Object.assign(this.BrazilData[2],{name:'Braziliex'});
-      Object.assign(this.BrazilData[3],{name:'flowBTC'});
-      Object.assign(this.BrazilData[4],{name:'Foxbit'});
-      Object.assign(this.BrazilData[5],{name:'LocalBitcoins'});
-      Object.assign(this.BrazilData[6],{name:'Mercado Bitcoin'});
-      Object.assign(this.BrazilData[7],{name:'Negocie Coins'});
+    this.http.get('https://www.mercadobitcoin.net/api/BTC/ticker/').subscribe(data => {
+      let mercadobitcoin = (data.json()).ticker;
+      this.BrazilData.push({name:'Mercado Bitcoin',high:mercadobitcoin.high,low:mercadobitcoin.low,vol:mercadobitcoin.vol,last:mercadobitcoin.last,buy:mercadobitcoin.buy,sell:mercadobitcoin.sell});
     });
 
-    this.http.get('https://cors-anywhere.herokuapp.com/https://api.bitvalor.com/v1/exchanges.json').subscribe(data => {
-      let dataGeral = data.json();
-      console.log(dataGeral)
-      this.BrazilFee.push(dataGeral.ARN);
-      this.BrazilFee.push(dataGeral.B2U);
-      this.BrazilFee.push(dataGeral.BAS);
-      this.BrazilFee.push(dataGeral.BIV);
-      this.BrazilFee.push(dataGeral.BSQ);
-      this.BrazilFee.push(dataGeral.BTD);
-      this.BrazilFee.push(dataGeral.BZX);
-      this.BrazilFee.push(dataGeral.FLW);
-      this.BrazilFee.push(dataGeral.FOX);
-      this.BrazilFee.push(dataGeral.LOC);
-      this.BrazilFee.push(dataGeral.MBT);
-      this.BrazilFee.push(dataGeral.NEG);
-      this.BrazilFee.push(dataGeral.PAX);
+    this.http.get('https://api.flowbtc.com:8405/GetTicker/BTCBRL/').subscribe(data => {
+      let flowbtc = data.json();
+      this.BrazilData.push({name:'Flow BTC',high:flowbtc.high,low:flowbtc.low,vol:flowbtc.volume,last:flowbtc.last,buy:flowbtc.ask,sell:flowbtc.bid});
     });
 
+
+    this.http.get('https://broker.negociecoins.com.br/api/v3/btcbrl/ticker').subscribe(data => {
+      let negociecoins = data.json();
+      this.BrazilData.push({name:'Negocie Coins',high:negociecoins.high,low:negociecoins.low,vol:negociecoins.vol,last:negociecoins.last,buy:negociecoins.buy,sell:negociecoins.sell});
+    });
+
+    this.http.get('https://api.bitcointrade.com.br/v1/public/BTC/ticker/').subscribe(data => {
+      let bitcointrade = data.json();
+      this.BrazilData.push({name:'Bitcointrade',high:bitcointrade.data.high,low:bitcointrade.data.low,vol:bitcointrade.data.volume,last:bitcointrade.data.last,buy:bitcointrade.data.buy,sell:bitcointrade.data.sell});
+    });
+
+    this.http.get('https://www.bitcointoyou.com/api/ticker.aspx').subscribe(data => {
+      let bitcointoyou = data.json().ticker
+      console.log(bitcointoyou)
+      this.BrazilData.push({name:'BitcoinToYou',high:bitcointoyou.high,low:bitcointoyou.low,vol:bitcointoyou.vol,last:bitcointoyou.last,buy:bitcointoyou.buy,sell:bitcointoyou.sell});
+    });
 
   }
 
