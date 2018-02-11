275111e90b3264ecc16c17bbd3bb34ee2dde7b39
Vinicius <vsampaio@ice.ufjf.br>
2018-02-11 21:08:47 -0200

    improvements in style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { setInterval } from 'timers';
 declare var jQuery:any;
 declare var $:any;
 @Component({
@@ -8,11 +10,15 @@ declare var $:any;
 })
 
 export class AppComponent {
+  date;
   title = 'app works!';
   constructor(){
     $( document ).ready(function(){
       $(".button-collapse").sideNav();
-    });   
+    });
+    setInterval(()=>{
+      this.date = moment().format('DD/MM/YYYY - HH:mm:ss');
+    },1);
   }
 
   //hide menu after click
