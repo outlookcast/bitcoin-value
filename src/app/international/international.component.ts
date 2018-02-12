350d200b1f9cb4e182386406e287b85865bcd736
Vinicius <vsampaio@ice.ufjf.br>
2018-02-12 00:22:19 -0200

    added poloniex on internation page and hotfix

--- src/app/international/international.component.ts
+++ src/app/international/international.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http } from '@angular/http/src/http';
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-international',
   templateUrl: './international.component.html',
@@ -7,8 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InternationalComponent implements OnInit {
 
-  constructor() {}
+  PoloniexData:any[] = [];
+  PoloniexVol:any[] = [];
 
+  constructor(private http: Http) {
+    this.http.get('https://poloniex.com/public?command=returnTicker').subscribe(data => {
+      let poloniex = data.json();
+      for (var key in poloniex) {
+        Object.assign(poloniex[key],{name:key});
+        poloniex[key]['percentChange'] = Number(poloniex[key]['percentChange']);
+        this.PoloniexData.push(poloniex[key]);
+      }
+    });
+
+    this.http.get('https://poloniex.com/public?command=return24hVolume').subscribe(data => {
+      let poloniex = data.json();
+      console.log(poloniex);
+    });
+  }
+
+  verify(number){
+    if(number<0){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  aprox(num){
+    return num.toFixed(2);
+  }
   ngOnInit() {
   }
 
